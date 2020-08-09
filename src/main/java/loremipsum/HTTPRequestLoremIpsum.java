98c63b66a444a9dfdaccabd24d3a51d4039c835d
unknown <darques@cifpfbmoll.eu>
2020-08-09 19:52:09 +0200

    feat(HTTPRequest): Almacenar parrafos del request en una lista

--- src/main/java/loremipsum/HTTPRequestLoremIpsum.java
+++ src/main/java/loremipsum/HTTPRequestLoremIpsum.java
@@ -1,19 +1,29 @@
 package loremipsum;
 
 import java.io.IOException;
-
+import java.util.ArrayList;
+import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+/** Clase encargada de hacer la llamada HTTP a lipsum.com y extraer los párrafos **/
+
 public class HTTPRequestLoremIpsum {
+	
+	public static List<String> paragraphs = new ArrayList<String>();
 
 	public static void main(String[] args) throws IOException {
 
 		Document doc = Jsoup.connect("https://es.lipsum.com/feed/html").get();
 		Elements elements = doc.select("#lipsum p");
 
-		System.out.println(elements);
+		for (Element element : elements) {
+			paragraphs.add(element.text());
+			System.out.println(element.text());
+		}
+		System.out.println(paragraphs);
 	}
 }
